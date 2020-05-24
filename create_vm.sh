b961a13ffd7b24a3a403bbdee46794a148092207
prachigupta307 <59722180+prachigupta307@users.noreply.github.com>
2020-05-24 22:32:38 +0530

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -4,6 +4,5 @@ ansible-playbook ./ansible_playbooks/creating_vm.yml -vvv
 
 echo "<==========================wait for 10 minutes atleast or============================>"
 echo "<==========================write watch virsh list --all=============================>"
-echo "<===================and wait until state of vm changes to shutdown===================>"
+echo "<===================and wait until state of vm changes to shutoff===================>"
 
-#watch virsh list --all
