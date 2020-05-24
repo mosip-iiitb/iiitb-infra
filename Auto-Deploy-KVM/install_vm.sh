1a4b350f1b90feb7cf1c562b2772ec62d4e2a98c
Prasanna Kumar <PrasannaKumar.Nimbagallu@iiitb.org>
2020-05-24 21:55:14 +0530

    Comments are added in install_vm.sh

--- Auto-Deploy-KVM/install_vm.sh
+++ Auto-Deploy-KVM/install_vm.sh
@@ -1,15 +1,23 @@
 
-
+#............Gets the host ip address............
 hostIp=$(hostname -I | awk '{print $1}')
+
+
+#............Copying updated ks.cfg in apache server............ 
 
 cp ks.cfg /var/www/html/install/ks.cfg
 
-systemctl reload apache2
-systemctl start apache2
+#............Restart the apache server............
+systemctl restart apache2
+
+
+#............Taking the inputs from command line for #VMs, VM prefix name and hardware configuration file path............
 
 number_of_vms=$1
 vm_prefix_name=$2
 hw_cfg_file_path=$3
+
+#............Validation for command line arguments............
 
 if [ ! $# -eq 3 ]
 then
@@ -18,24 +26,33 @@ then
 	exit
 fi
 
+#............Checking whether hardware config file path is valid or not............
+
 if [ ! -e $hw_cfg_file_path ]
 then
 	echo "Provide the correct path for vm hardware configuration file."$#
 	exit
 fi
+
+#............Fetching the hardware configuration from the configuration file............
+
 CPUS=$( egrep "<CPUS>" $hw_cfg_file_path | awk '{ print $2 }')
 RAM=$( egrep "<RAM>" $hw_cfg_file_path | awk '{ print $2 }')
 SIZE=$( egrep "<SIZE>" $hw_cfg_file_path | awk '{ print $2 }')
 ISO_PATH=$( egrep "<ISO_PATH>" $hw_cfg_file_path | awk '{ print $2 }')
 
 VM_NAME=$vm_prefix_name"1"
-IMAGE_NAME=$VM_NAME"-os.qcow2"
+IMAGE_FORMAT=".qcow2"
+
+#............Validates the location of .iso file............
 
 if [ ! -e $ISO_PATH ]
 then
 	echo "Provide the location of iso file."
 	exit
 fi
+
+#............Virtual machine installation............
 
 iterate=0
 while [ $iterate -lt $number_of_vms ]
@@ -46,7 +63,7 @@ do
 			--os-variant=centos7.0 \
 			--ram=$RAM \
 			--vcpus=$CPUS  \
- 			--disk path=/var/lib/libvirt/images/$vm_prefix_name$iterate,format=qcow2,bus=virtio,size=$SIZE \
+ 			--disk path=/var/lib/libvirt/images/$vm_prefix_name$iterate$IMAGE_FORMAT,format=qcow2,bus=virtio,size=$SIZE \
   			--graphics none \
 		      	--location=$ISO_PATH \
   			--extra-args="console=tty0 console=ttyS0,115200"  -x ks=http://$hostIp/install/ks.cfg \
